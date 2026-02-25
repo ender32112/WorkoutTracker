70a1d2d3bec17d52a1a40046ce06d335ee651b78
ender32112 <73356846+ender32112@users.noreply.github.com>
2026-02-26 00:22:41 +0300

    забыл зависимость :((

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -136,5 +136,6 @@ dependencies {
     implementation("androidx.camera:camera-camera2:1.4.1")
     implementation("androidx.camera:camera-lifecycle:1.4.1")
     implementation("androidx.camera:camera-view:1.4.1")
+    implementation("com.google.guava:guava:33.3.1-android")
 
 }
